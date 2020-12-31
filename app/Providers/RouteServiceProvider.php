1d28c1dd12a12ed0c4c3fef7660f80af72e6fb02
santiago chiluisa <santoch93@hotmail.com>
2020-12-30 21:13:42 -0500

    nuevo controlador

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/zapatos';
 
     /**
      * Define your route model bindings, pattern filters, etc.
--- resources/views/zapatos/index.blade.php
+++ resources/views/zapatos/index.blade.php
@@ -1,4 +1,7 @@
+@extends('layouts.app')
+@section('content')
 <h1>zapatos en oferta</h1>
 @foreach ($zapatos as $zapato)
     <li>{{$zapato}}</li>
 @endforeach
+@endsection
